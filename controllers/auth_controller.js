b28bb61114f706cf332bc2d56aa0ae4922069126
0xgeeky <techiebishwas@gmail.com>
2024-09-29 14:01:57 +0545

    Feat:Added jwt token

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -11,10 +11,13 @@ const register_controller = async (req, res, next) => {
 
   const user = await User.create({ first_name, email, password });
 
+  const token = user.createJWT();
+
   return res.status(200).send({
     success: true,
     messgae: "User created succesfully",
     user: user,
+    token,
   });
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
 
 // scheme
 const user_schema = new mongoose.Schema(
@@ -28,6 +30,20 @@ const user_schema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// middlewares
+user_schema.pre("save", async function () {
+  // password hashing
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// JSON WEBTOKEN
+user_schema.methods.createJWT = function () {
+  return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 const User = mongoose.model("user", user_schema);
 
